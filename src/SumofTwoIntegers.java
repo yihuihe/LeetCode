40c4028607066c7357675f530ba1b0bf0f913cd2
yihuihe <yhhe413@163.com>
2016-11-11 15:52:34 +0800

    SumOfTwoIntegrs

---
+++ src/SumofTwoIntegers.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class SumofTwoIntegers {
+
+	public static int getSum(int a,int b){
+		int result;
+		int xor,and;
+		if ((a & b )!=0)
+		{
+		
+			while( (a & b)!=0)
+			{
+				xor = a ^ b;
+				and = (a & b)<<1;
+				
+				a = and;
+				b = xor;	
+			}
+			result = a ^ b;
+		}
+		else 
+		result = a ^ b;	
+		return result;
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int x = getSum(a,b);
+		System.out.println(x);
+	}
+
+}
