02a03f977e951947db0d57ee1deb856c36554fa6
yihuihe <yhhe413@163.com>
2017-02-15 23:25:12 +0800

    DeleteNode --- 237 in Leetcode

---
+++ src/DeleteNode.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class DeleteNode {
+
+	public class ListNode {
+		     int val;
+		     ListNode next;
+		    public ListNode(int x) { val = x; }
+		  }
+	public static void deleteNode(ListNode node){
+		node.val = node.next.val;
+		node.next = node.next.next;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
