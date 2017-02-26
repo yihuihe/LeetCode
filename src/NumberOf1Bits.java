9cb4de7fa6e11c536fe3784504d688a312f67410
yihuihe <yhhe413@163.com>
2017-02-26 16:08:53 +0800

    Number of 1 bits 191 in leetcode

--- src/NumberOf1Bits.java
+++ src/NumberOf1Bits.java
@@ -1,24 +1,17 @@
 
 public class NumberOf1Bits {
-	public static long hammingWeight(int n) {
+	public static int hammingWeight(int n) {
         int count = 0;
-        /*
+
         for(int i=0;i<32;i++){
-        	if(((n >> i) & 1 ) ==1)
+        	if(((n >>> i) & 1 ) ==1)
         		count++;
-        }
-        */
-        long temp = n & 0xFFFFFFFF;
-        while(temp>0){
-        	if((temp & 1) == 1)
-        		count++;
-        	temp>>=1;
         }
         return count;
     }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(hammingWeight(2147483647));
+		System.out.println(hammingWeight(256));
 	}
 
 }
